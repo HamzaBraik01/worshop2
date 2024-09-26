9c097e2bd48eabf6111a9e955bbde629ed643973
Braik <hamzabraik02@gmail.com>
2024-09-26 16:48:53 +0100

    challenge 13

--- Challenge13.c
+++ Challenge13.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-int main() {
-    srand(time(NULL)); 
-    int jour = rand() % 7; 
+void main() {
+    float x1, y1, x2, y2, px, py;
 
-    switch (jour) {
-        case 0:
-            printf("Lundi\n");
-            break;
-        case 1:
-            printf("Mardi\n");
-            break;
-        case 2:
-            printf("Mercredi\n");
-            break;
-        case 3:
-            printf("Jeudi\n");
-            break;
-        case 4:
-            printf("Vendredi\n");
-            break;
-        case 5:
-            printf("Samedi\n");
-            break;
-        case 6:
-            printf("Dimanche\n");
-            break;
-        default:
-            printf("Erreur : jour invalide\n"); 
-            break;
+    printf("Entrez les coordonnées de A (x1, y1) : ");
+    scanf("%f %f", &x1, &y1);
+
+    printf("Entrez les coordonnées de B (x2, y2) : ");
+    scanf("%f %f", &x2, &y2);
+
+    printf("Entrez les coordonnées du point P (px, py) : ");
+    scanf("%f %f", &px, &py);
+
+    float determinant = (y2 - y1) * (px - x1) - (x2 - x1) * (py - y1);
+
+    if (determinant == 0) {
+        if ((px >= x1 && px <= x2 || px >= x2 && px <= x1) && 
+            (py >= y1 && py <= y2 || py >= y2 && py <= y1)) {
+            printf("Le point P se trouve sur le segment.\n");
+        } else {
+            printf("Le point P est aligné mais ne se trouve pas sur le segment.\n");
+        }
+    } else {
+        printf("Le point P ne se trouve pas sur le segment.\n");
     }
-
-    return 0;
 }
